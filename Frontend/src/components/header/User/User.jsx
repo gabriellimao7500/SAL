8af6ea4ed2d277819e43d427928c1a69917bf330
gabriellimao7500 <gabriellimao7500@gmail.com>
2024-06-05 22:53:36 -0300

    update

--- Frontend/src/components/header/User/User.jsx
+++ Frontend/src/components/header/User/User.jsx
@@ -1,19 +1,52 @@
-import './User.css'
-import Login from '../../Login/Login'
+import './User.css';
+import { useState, useEffect, useRef } from 'react';
+import Login from '../../Login/Login';
 
-function User(){
-    return(
+function User() {
+    const [login, setLogin] = useState(false);
+    const menuRef = useRef(null);
+    const sectionRef = useRef(null);
+
+    function toggleLogin() {
+        setLogin(prevLogin => !prevLogin);
+    }
+
+    function handleClickOutside(event) {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setLogin(false);
+        }
+    }
+
+    function handleSectionClick(event) {
+        // Verifica se o clique ocorreu fora do menu <Login>
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setLogin(false);
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
         <>
-            <div className='user'>
-                <div>your account</div>
+            <div onClick={toggleLogin} className='user'>
+                <div>Login</div>
                 <div className='userImage'></div>
             </div>
 
-            <section className='Login'>
-                <Login></Login>
-            </section>
+            {login && (
+                <section ref={sectionRef} className="Login" onClick={handleSectionClick}>
+                    <div ref={menuRef}>
+                        <Login />
+                    </div>
+                </section>
+            )}
         </>
-    )
+    );
 }
 
-export default User
+export default User;
