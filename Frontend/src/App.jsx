142de596f7138fe5b9cc3501c59659acbdf82374
gabriellimao7500 <gabriellimao7500@gmail.com>
2024-06-26 22:20:15 -0300

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+import SelectLab from './pages/SelectLab'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/SelectLab' element={<SelectLab />} />
       </Routes>
     </BrowserRouter>
   )
