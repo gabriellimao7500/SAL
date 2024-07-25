f56b68ad71a98fffad4dddb365f2657deeb698ee
GMarsura <biel.marsura@gmail.com>
2024-07-25 02:30:42 -0300

    update

--- Frontend/src/components/LabsSelect/LabsSelect.jsx
+++ Frontend/src/components/LabsSelect/LabsSelect.jsx
@@ -2,12 +2,13 @@ import styles from './LabsSelect.module.css'
 import { Link } from 'react-router-dom'
 import './button.css'
 
-function LabsSelect({svg, name}){
+function LabsSelect({svg, name, number}){
     
     return(
         <Link to="/Labs" className={styles.button}>
             <div dangerouslySetInnerHTML={{ __html: svg }} />
             <h3>{name}</h3>
+            <h3>{number}</h3>
         </Link>
     )
 }
