fe9d1bab7f17e999196213e44b5eab7cd1289657
Yan Carlos Camargo <73352444+YanCarlosCamargo@users.noreply.github.com>
2025-05-06 10:02:56 -0300

    Adicionado campo de busca

--- Frontend/src/pages/AdminTeachers.jsx
+++ Frontend/src/pages/AdminTeachers.jsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 
 const AdminTeachers = () => {
     const [teachers, setTeachers] = useState([]);
+    const [filteredTeachers, setFilteredTeachers] = useState([]); // Estado para professores filtrados
+    const [searchTerm, setSearchTerm] = useState(''); // Estado para o termo de busca
     const [form, setForm] = useState({ name: '', email: '' });
     const [editingId, setEditingId] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar o popup
@@ -14,6 +16,7 @@ const AdminTeachers = () => {
         try {
             const response = await axios.get('http://localhost:3333/teachers');
             setTeachers(response.data);
+            setFilteredTeachers(response.data); // Inicializa os professores filtrados
         } catch (error) {
             console.error('Erro ao buscar professores:', error);
         }
@@ -69,95 +72,117 @@ const AdminTeachers = () => {
         }
     };
 
+    // Função para filtrar professores com base no termo de busca
+    const handleSearch = (e) => {
+        const term = e.target.value.toLowerCase();
+        setSearchTerm(term);
+        setFilteredTeachers(
+            teachers.filter((teacher) =>
+                teacher.nome.toLowerCase().includes(term)
+            )
+        );
+    };
+
     useEffect(() => {
         fetchTeachers();
     }, []);
 
     return (
         <body>
-
-            <div>
-                <h1>Gerenciar Professores</h1>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', overflowY: 'auto', maxHeight: '700px' }}>
-                    <table style={{ flexDirection: 'row', flex: 1, alignItems: 'center' }}>
-                        <thead>
-                            <tr>
-                                <th>Nome</th>
-                                <th>Email</th>
-                                <th>Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody >
-                            {teachers.map((teacher) => (
-                                <tr key={teacher.id} >
-                                    <td>{teacher.nome}</td>
-                                    <td>{teacher.email}</td>
-                                    <td>
-                                        <button onClick={() => handleEdit(teacher)}>Editar</button>
-                                    </td>
+            <div className="admin-teachers-container">
+                {/* Campo de busca */}
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                    <h1>Gerenciar Professores</h1>
+                    <input
+                        type="text"
+                        placeholder="Buscar por nome"
+                        value={searchTerm}
+                        onChange={handleSearch}
+                        style={{ marginBottom: '20px', padding: '10px', width: '300px' }}
+                    />
+                    <div style={{ overflowY: 'auto', maxHeight: '700px' }} idClass='table'>
+                        <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+                            <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th>Ações</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-
-                {/* Popup para edição */}
-                {isModalOpen && (
-                    <div style={{
-                        position: 'fixed',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center'
-                    }}>
-                        <div style={{
-                            backgroundColor: 'white',
-                            padding: '20px',
-                            borderRadius: '8px',
-                            width: '400px',
-                            textAlign: 'center'
-                        }}>
-                            <h2>Editar Professor</h2>
-                            <form onSubmit={handleSubmit}>
-                                <input
-                                    type="text"
-                                    placeholder="Nome"
-                                    value={form.name}
-                                    onChange={(e) => setForm({ ...form, name: e.target.value })}
-                                    required
-                                />
-                                <input
-                                    type="email"
-                                    placeholder="Email"
-                                    value={form.email}
-                                    onChange={(e) => setForm({ ...form, email: e.target.value })}
-                                    required
-                                />
-                                <button type="submit">Salvar</button>
-                                <button type="button" onClick={closeModal} style={{ marginLeft: '10px' }}>Cancelar</button>
-                            </form>
-                            <button
-                                onClick={handleDelete}
-                                style={{
-                                    marginTop: '20px',
-                                    backgroundColor: 'red',
-                                    color: 'white',
-                                    border: 'none',
-                                    padding: '10px 20px',
-                                    borderRadius: '5px',
-                                    cursor: 'pointer'
-                                }}
-                            >
-                                Excluir
-                            </button>
-                        </div>
+                            </thead>
+                            <tbody >
+                                {filteredTeachers.map((teacher) => (
+                                    <tr key={teacher.id} >
+                                        <td>{teacher.nome}</td>
+                                        <td>{teacher.email}</td>
+                                        <td>
+                                            <button onClick={() => handleEdit(teacher)}>Editar</button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
-                )}
+
+
+                    {/* Popup para edição */}
+                    {isModalOpen && (
+                        <div style={{
+                            position: 'fixed',
+                            top: 0,
+                            left: 0,
+                            width: '100%',
+                            height: '100%',
+                            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                            <div style={{
+                                backgroundColor: 'white',
+                                padding: '20px',
+                                borderRadius: '8px',
+                                width: '400px',
+                                textAlign: 'center'
+                            }}>
+                                <h2>Editar Professor</h2>
+                                <form onSubmit={handleSubmit}>
+                                    <input
+                                        type="text"
+                                        placeholder="Nome"
+                                        value={form.name}
+                                        onChange={(e) => setForm({ ...form, name: e.target.value })}
+                                        required
+                                    />
+                                    <input
+                                        type="email"
+                                        placeholder="Email"
+                                        value={form.email}
+                                        onChange={(e) => setForm({ ...form, email: e.target.value })}
+                                        required
+                                    />
+                                    <button type="submit">Salvar</button>
+                                    <button type="button" onClick={closeModal} style={{ marginLeft: '10px' }}>Cancelar</button>
+                                </form>
+                                <button
+                                    onClick={handleDelete}
+                                    style={{
+                                        marginTop: '20px',
+                                        backgroundColor: 'red',
+                                        color: 'white',
+                                        border: 'none',
+                                        padding: '10px 20px',
+                                        borderRadius: '5px',
+                                        cursor: 'pointer'
+                                    }}
+                                >
+                                    Excluir
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
+
         </body>
     );
 };
