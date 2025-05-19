fc3949d14bc70123de3dd607f6cea9e6fe21a75c
Yan Carlos Camargo <73352444+YanCarlosCamargo@users.noreply.github.com>
2025-05-19 10:52:49 -0300

    Design tela de Atualização em Massa

--- Frontend/src/pages/MassUpdate.jsx
+++ Frontend/src/pages/MassUpdate.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import config from "../../config"; // Importa o arquivo de configuração
-//import './MassUpdate.css';
+import './MassUpdate.css';
 
 function MassUpdate() {
     const [sqlCode, setSqlCode] = useState("");
@@ -39,6 +39,8 @@ function MassUpdate() {
 
     const handleReject = (conflictId) => {
         console.log("Conflito rejeitado:", conflictId);
+
+        
         // Marca o conflito como rejeitado
         const updatedResults = comparisonResults.map(item =>
             item.conflictId === conflictId ? { ...item, accepted: false } : item
@@ -65,45 +67,56 @@ function MassUpdate() {
     };
 
     return (
-        <div className="mass-update-page">
-            <h1>Atualizar Tabela em Massa</h1>
+        <div className="mass-update-container">
+            <h1 className="mass-update-title">Atualizar Tabela em Massa</h1>
             <textarea
                 value={sqlCode}
                 onChange={handleSqlChange}
                 placeholder="Insira o código SQL aqui..."
             />
-            <button onClick={handleCompare} disabled={loading}>
+            <button onClick={handleCompare} disabled={loading} className="mass-update-action-btn">
                 {loading ? "Comparando..." : "Comparar"}
             </button>
+            <h2>Resultados da Comparação</h2>
+            <p>{`Total de Conflitos Encontrados: ${comparisonResults.length}`}</p>
             {comparisonResults.length > 0 && (
-                <ul>
-                    {comparisonResults.map(result => (
-                        <li key={result.conflictId}>
-                            <p>{`Conflito de Horários`}</p>
-                            <p><strong>Reserva Existente:</strong></p>
-                            <p>{`Laboratório: ${result.existingRes.idLaboratorio}`}</p>
-                            <p>{`Data: ${result.existingRes.dataReserva}`}</p>
-                            <p>{`Período: ${result.existingRes.periodo}`}</p>
-                            <p>{`Aula: ${result.existingRes.aulaReserva}`}</p>
-                            <p>{`Professor: ${result.existingRes.idProfessor}`}</p>
-                            <p>{`Descrição: ${result.existingRes.motivo}`}</p>
-                            <p><strong>Nova Reserva:</strong></p>
-                            <p>{`Laboratório: ${result.newRes.idLaboratorio}`}</p>
-                            <p>{`Data: ${result.existingRes.dataReserva}`}</p>
-                            <p>{`Período: ${result.newRes.periodo}`}</p>
-                            <p>{`Aula: ${result.newRes.aulaReserva}`}</p>
-                            <p>{`Professor: ${result.newRes.idProfessor}`}</p>
-                            <p>{`Descrição: ${result.newRes.motivo}`}</p>
-                            <button onClick={() => handleAccept(result.conflictId)}>Aceitar</button>
-                            <button onClick={() => handleReject(result.conflictId)}>Rejeitar</button>
-                        </li>
-                    ))}
-                </ul>
+                <div className="mass-update-cards">
+
+
+                    {
+                        comparisonResults.map(result => (
+
+                            <div className={`mass-update-card ${result.accepted === true ? "accepted" : result.accepted === false ? "rejected" : ""}`} key={result.conflictId}>
+                                <h3>{`Conflito de Horários`}</h3>
+                                <h4>Reserva Existente:</h4>
+                                <p>{`Laboratório: ${result.existingRes.idLaboratorio}`}</p>
+                                <p>{`Data: ${result.existingRes.dataReserva.split('T')[0]}`}</p>
+                                <p>{`Período: ${result.existingRes.periodo}`}</p>
+                                <p>{`Aula: ${result.existingRes.aulaReserva}`}</p>
+                                <p>{`Professor: ${result.existingRes.idProfessor}`}</p>
+                                <p>{`Descrição: ${result.existingRes.motivo}`}</p>
+                                <p><strong>Nova Reserva:</strong></p>
+                                <p>{`Laboratório: ${result.newRes.idLaboratorio}`}</p>
+                                <p>{`Data: ${result.existingRes.dataReserva.split('T')[0]}`}</p>
+                                <p>{`Período: ${result.newRes.periodo}`}</p>
+                                <p>{`Aula: ${result.newRes.aulaReserva}`}</p>
+                                <p>{`Professor: ${result.newRes.idProfessor}`}</p>
+                                <p className="mass-update-card-info">{`Descrição: ${result.newRes.motivo}`}</p>
+
+                                <button onClick={() => handleAccept(result.conflictId)} className="mass-update-action-btn">Aceitar</button>
+
+
+                                <button onClick={() => handleReject(result.conflictId)} className="mass-update-action-btn-reject">Rejeitar</button>
+                            </div>
+
+                        ))
+                    }
+                </div>
             )}
             <button onClick={handleResolve} disabled={comparisonResults.length === 0}>
                 Resolver Conflitos
             </button>
-        </div>
+        </div >
     );
 }
 
